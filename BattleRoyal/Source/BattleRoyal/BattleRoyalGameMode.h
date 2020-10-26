bfe873caafb90013455ef59fe378f2f498f5a661
pdj2428 <49777470+pdj2428@users.noreply.github.com>
2020-10-26 23:43:02 +0900

    Create Map_1
    .

---
+++ BattleRoyal/Source/BattleRoyal/BattleRoyalGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleRoyalGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABattleRoyalGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABattleRoyalGameMode();
+};
+
+
+
